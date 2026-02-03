e21fbfe74cf62e01b2ec766c4b75c777661b9f2d
Dimazoro Chinonso <chinonsomarcelino81@gmail.com>
2026-02-03 10:59:47 +0100

    update

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,13 +1,15 @@
+import { auth } from '@/auth'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+    const session = await auth()
     return (
         <main className='max-md:text-sm md:text-base'>
             {/* HERO */}
             <section className="bg-[url('/tech.bg.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
-                <div className="bg-[#0F172A]/50 text-white min-h-dvh">
+                <div className="bg-[#0F172A]/50 text-white min-h-screen">
                     <div className="max-w-7xl mx-auto md:px-6 py-10 md:py-16 lg:py-24 text-center justify-center px-4">
-                        <h1 className="text-3xl lg:text-6xl font-bold leading-tight mt-20 md:mt-10 md:text-4xl">
+                        <h1 className="text-2xl lg:text-6xl font-bold leading-tight mt-20 md:mt-10 md:text-4xl">
                             Organize, Share & Discover <br />
                             <span className="text-[#55fff6]">Real-World Cases</span>
                         </h1>
@@ -18,12 +20,23 @@ const page = () => {
                         </p>
 
                         <div className="mt-10 flex justify-center gap-4 max-md:flex-col ">
+                            {
+                                session ? 
                             <a
+                                href="/signup"
+                                className="bg-[#F97316] text-[#0F172A] px-4 md:px-8 py-2 md:py-3 rounded-lg font-semibold hover:opacity-90 transition hover:-translate-y-2 duration-300"
+                            >
+                                Write a case
+                            </a>
+                                
+                                :
+                                <a
                                 href="/signup"
                                 className="bg-[#F97316] text-[#0F172A] px-4 md:px-8 py-2 md:py-3 rounded-lg font-semibold hover:opacity-90 transition hover:-translate-y-2 duration-300"
                             >
                                 Get Started
                             </a>
+                            }
 
                             <a
                                 href="#features"
@@ -71,7 +84,7 @@ const page = () => {
             <section className="bg-[url('/tech.bg.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
                 <main className='bg-[#0F172A]/50'>
                     <div className='text-white py-24 text-center '>
-                        <h2 className="text-3xl md:text-4xl font-bold">
+                        <h2 className="text-2xl md:text-4xl font-bold">
                             Start Exploring Real Cases Today
                         </h2>
 
@@ -80,12 +93,22 @@ const page = () => {
                         </p>
 
                         <div className="mt-8">
+                            {
+                                session ?
+                                <a
+                                href="/explore"
+                                className="bg-[#F97316] text-[#0F172A] px-10 py-3 rounded-lg font-semibold hover:opacity-90 transition"
+                            >
+                                Explore Cases
+                            </a>
+                                :
                             <a
                                 href="/signup"
                                 className="bg-[#F97316] text-[#0F172A] px-10 py-3 rounded-lg font-semibold hover:opacity-90 transition"
                             >
                                 Create Free Account
                             </a>
+                            }
                         </div>
                     </div>
                 </main>
